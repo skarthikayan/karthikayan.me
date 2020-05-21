46257872f46251b6775a5ba3b4f7897aa17980a7
Karthikayan Selvaraj <skarthikayan7@gmail.com>
2020-05-21 14:08:23 +0530

    Added docs folder

---
+++ docs/precache-manifest.65c391f3faf33a3c3ff550088c80ff03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69722427049fa9f3b507f68ee7acca36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81cd0db2cd826a18df37",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dc4d0b14d83f22dbc4f0",
+    "url": "/static/js/2.2b6ae07a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2b6ae07a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81cd0db2cd826a18df37",
+    "url": "/static/js/main.467d8693.chunk.js"
+  },
+  {
+    "revision": "58554747a7ec5b555346",
+    "url": "/static/js/runtime-main.b42205e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
