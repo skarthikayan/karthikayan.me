5a532d34fd8f42647019ac82de52b818274593d2
Karthikayan Selvaraj <skarthikayan7@gmail.com>
2020-05-21 14:02:35 +0530

    Build

---
+++ precache-manifest.51136581520ccda30e8f34d37eabe469.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a3ad75a313da5abb2b653d101128a7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac72d7ba5907c3295bcd",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "35134ded6ce09598bb6f",
+    "url": "/static/js/2.16b68ede.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.16b68ede.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac72d7ba5907c3295bcd",
+    "url": "/static/js/main.9a5e6c9e.chunk.js"
+  },
+  {
+    "revision": "1c9d26ce377364cd5bac",
+    "url": "/static/js/runtime-main.a11a7f8d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
